cb573794d76a28a0e5ca89833f57c520c7490386
Vincent <vincent@getseoshop.com>
2017-06-25 15:00:17 +0200

    Drawing board

--- eight-queens.js
+++ eight-queens.js
@@ -17,8 +17,39 @@ class EightQueens {
 
     var context = this.canvas.getContext("2d");
 
-    context.fillStyle = '#666666'
+    let blockSize = 50;
+
+    for (var i = 0; i < 8; i++) {
+        context.fillStyle = '#666';
+        if (i % 2)
+        {
+          context.fillStyle = '#fFF';
+        }
+
+        if (i > 7)
+        {
+          context.fillStyle = '#ff0000';
+        }
+
+        context.fillRect( i * blockSize, 0, blockSize, blockSize);
+        context.fillStyle = '#ff0000';
+        context.fillText('NR ' + (i+1), i * (blockSize), blockSize/2);
+    }
+
+    // context.fillStyle = '#666666';
+    // context.fillRect( 0, 0, 100, 100);
+    // context.fillStyle = '#777';
+    // context.fillRect( 100, 0, 100, 100);
+    // context.fillStyle = '#666666';
+    // context.fillRect( 200, 0, 100, 100);
+    // context.fillStyle = '#777';
+    // context.fillRect( 300, 0, 100, 100);
+    // context.fillStyle = '#666666';
+    // context.fillRect( 400, 0, 100, 100);
     context.save();
+
+
+    console.log(context);
 
     document.body.appendChild(this.canvas);
   }
