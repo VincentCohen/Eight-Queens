776f566c55f788c7bab0dc8fbf26f91edc9ae7ef
Vincent <vincent@getseoshop.com>
2017-06-26 13:55:11 +0200

    Cleaned up a  little stuff

--- eight-queens.js
+++ eight-queens.js
@@ -1,7 +1,7 @@
 class EightQueens {
 
   constructor() {
-    this.canvasId = 'board';
+    this.canvasId = 'board'
     this.canvasWidth = 400
     this.canvasHeight = 400
 
@@ -11,53 +11,40 @@ class EightQueens {
 
   board ()
   {
-    this.canvas = document.createElement("canvas");
-    this.canvas.width     = this.canvasHeight;
-    this.canvas.height    = this.canvasWidth;
+    this.canvas = document.createElement("canvas")
+    this.canvas.width = this.canvasHeight
+    this.canvas.height = this.canvasWidth
 
-    var context = this.canvas.getContext("2d");
-    var black = '#826850';
-    var white = '#fff';
+    var context = this.canvas.getContext("2d")
+    var black = '#826850'
+    var white = '#fff'
 
-    let blockSize = 50;
+    let blockSize = 50
+
     for (var y = 0; y < 8; y++) {
       for (var x = 0; x < 8; x++) {
-          var fillColor = black;
-          if (x % 2)
-          {
-            fillColor = white;
+          var fillColor = black
+          if (x % 2) {
+            fillColor = white
           }
 
-          if ( (y % 2) == 1)
-          {
-            fillColor = white;
-            if (x % 2)
-            {
-              fillColor = black;
+          if ((y % 2) == 1){
+            fillColor = white
+            if (x % 2) {
+              fillColor = black
             }
           }
 
-          context.fillStyle = fillColor;
-          context.fillRect( x * blockSize, y * blockSize, blockSize, blockSize);
-          context.fillStyle = '#ff0000';
-          context.fillText('X' + x + ' Y' + y, x * blockSize, y * blockSize);
+          context.fillStyle = fillColor
+          context.fillRect( x * blockSize, y * blockSize, blockSize, blockSize)
+
+          // debug
+          context.fillStyle = '#ff0000'
+          context.fillText('X' + x + ' Y' + y, x * blockSize, y * blockSize)
       }
     }
 
-    // context.fillStyle = '#666666';
-    // context.fillRect( 0, 0, 100, 100);
-    // context.fillStyle = '#777';
-    // context.fillRect( 100, 0, 100, 100);
-    // context.fillStyle = '#666666';
-    // context.fillRect( 200, 0, 100, 100);
-    // context.fillStyle = '#777';
-    // context.fillRect( 300, 0, 100, 100);
-    // context.fillStyle = '#666666';
-    // context.fillRect( 400, 0, 100, 100);
     context.save();
-
-
-    console.log(context);
 
     document.body.appendChild(this.canvas);
   }
